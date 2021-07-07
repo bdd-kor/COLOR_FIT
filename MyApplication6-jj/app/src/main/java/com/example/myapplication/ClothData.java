73bd57805b89d935585ca2c9138a91ecdd30d397
yeoang <wjdwnd369@gmail.com>
2021-07-07 16:31:05 +0900

    app search upload

---
+++ MyApplication6-jj/app/src/main/java/com/example/myapplication/ClothData.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import java.util.Locale;
+
+public class ClothData {
+    private String goods_image;
+    private String goods_name;
+    private String goods_price;
+
+    public String getGoods_image() {
+        return goods_image;
+    }
+
+    public void setGoods_image(String goods_image) {
+        this.goods_image = goods_image;
+    }
+
+
+
+    public String getGoods_name() {
+        return goods_name;
+    }
+
+    public void setGoods_name(String goods_name) {
+        this.goods_name = goods_name;
+    }
+
+
+    public String getGoods_price() {
+        return goods_price;
+    }
+
+    public void setGoods_price(String goods_price) {
+        this.goods_price = goods_price;
+    }
+
+
+}
---
+++ MyApplication6-jj/app/src/main/java/com/example/myapplication/StartActivity.java
@@ -0,0 +1,54 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_page);
+
+        Button btnSTART = findViewById(R.id.rear_camera);
+        TextView tvPASS = findViewById(R.id.tvPASS);
+
+        btnSTART.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Intent intent = new Intent(StartActivity.this, CameraActivity.class);
+                intent.putExtra("mode", 0);
+                startActivity(intent);
+            }
+        });
+
+        tvPASS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
