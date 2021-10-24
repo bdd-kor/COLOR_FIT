dfff8de17ef5e48a103be064d3fa9716f9de0f49
yeoang <wjdwnd369@gmail.com>
2021-10-24 16:42:42 +0900

    33

---
+++ jj/color_fit/app/src/main/java/com/example/color_fit/CameraActivity.java
@@ -0,0 +1,234 @@
+package com.example.color_fit;
+
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import androidx.core.content.FileProvider;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
+
+import static android.os.Environment.DIRECTORY_PICTURES;
+
+public class CameraActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 672;
+    private String imageFilePath;
+    private Uri photoUri;
+    private MediaScanner mMediaScanner; // 사진 저장 시 갤러리 폴더에 바로 반영사항을 업데이트 시켜주려면 이 것이 필요하다(미디어 스캐닝)
+    private int season = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        // 사진 저장 후 미디어 스캐닝을 돌려줘야 갤러리에 반영됨.
+        mMediaScanner = MediaScanner.getInstance(getApplicationContext());
+
+        // 권한 체크
+        TedPermission.with(getApplicationContext())
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage("카메라 권한이 필요합니다.")
+                .setDeniedMessage("거부하셨습니다.")
+                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+                .check();
+
+        /************* Php script path ****************/
+        //서버컴퓨터의 ip주소
+        findViewById(R.id.btn_capture).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException e) {
+
+                    }
+
+                    if (photoFile != null) {
+                        photoUri = FileProvider.getUriForFile(getApplicationContext(), getPackageName(), photoFile);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+//                        intent.putExtra("android.intent.extras.LENS_FACING_FRONT", 1);
+//                        intent.putExtra("android.intent.extras.USE_FRONT_CAMERA", true);
+                        intent.putExtra("android.intent.extras.CAMERA_FACING", 1);
+                        // 전면카메라 사용
+                        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                    }
+
+                }
+
+            }
+        });
+
+        //다음 버튼
+        findViewById(R.id.button_next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), q1.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "TEST_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".png",
+                storageDir
+        );
+        imageFilePath = image.getAbsolutePath();
+        return image;
+    }
+
+    //저장경로 -- 내장메모리/Pictures/COLORFIT/pcimage.png
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bitmap bitmap = BitmapFactory.decodeFile(imageFilePath);
+            ExifInterface exif = null;
+            try {
+                exif = new ExifInterface(imageFilePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            int exifOrientation;
+            int exifDegree;
+            if (exif != null) {
+                exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                exifDegree = exifOrientationToDegress(exifOrientation);
+            } else {
+                exifDegree = 0;
+            }
+            String result = "";
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HHmmss", Locale.getDefault());
+            Date curDate = new Date(System.currentTimeMillis());
+//            String filename = formatter.format(curDate);
+            String filename = "pcimage";
+
+            String strFolderName = Environment.getExternalStoragePublicDirectory(DIRECTORY_PICTURES) + File.separator + "COLORFIT" + File.separator;
+            File file = new File(strFolderName);
+            if (!file.exists())
+                file.mkdirs();
+
+            File f = new File(strFolderName + "/" + filename + ".png");
+            result = f.getPath();
+
+            FileOutputStream fOut = null;
+            try {
+                fOut = new FileOutputStream(f);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                result = "Save Error fOut";
+            }
+
+            // 비트맵 사진 폴더 경로에 저장
+            rotate(bitmap, exifDegree).compress(Bitmap.CompressFormat.PNG, 70, fOut);
+
+            try {
+                fOut.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                fOut.close();
+                // 방금 저장된 사진을 갤러리 폴더 반영 및 최신화
+                mMediaScanner.mediaScanning(strFolderName + "/" + filename + ".png");
+            } catch (IOException e) {
+                e.printStackTrace();
+                result = "File close Error";
+            }
+
+            // 이미지 뷰에 비트맵을 set하여 이미지 표현
+//            ((ImageView) findViewById(R.id.iv_result)).setImageBitmap(rotate(bitmap, exifDegree));
+
+
+ /*           //서버업로드
+            dialog = ProgressDialog.show(CameraActivity.this, "", "Uploading file...", true);
+            new Thread(new Runnable() {
+                public void run() {
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            messageText.setText("uploading started.....");
+                        }
+                    });
+                    uploadFile(uploadFilePath + "" + uploadFileName);
+                }
+            }).start();
+*/
+
+
+        }
+    }
+
+    private int exifOrientationToDegress(int exifOrientation) {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270;
+        }
+        return 0;
+    }
+
+    private Bitmap rotate(Bitmap bitmap, float degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+
+    PermissionListener permissionListener = new PermissionListener() {
+        @Override
+        public void onPermissionGranted() {
+            Toast.makeText(getApplicationContext(), "권한이 허용됨",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+            Toast.makeText(getApplicationContext(), "권한이 거부됨",Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
+
+
+}
---
+++ jj/color_fit/app/src/main/java/com/example/color_fit/FilterActivity.java
@@ -0,0 +1,119 @@
+package com.example.color_fit;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.annotation.IdRes;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class FilterActivity extends AppCompatActivity{
+
+    private RadioGroup PcGroup, CgGroup;
+    private RadioButton rdSpring, rdSummer, rdAutumn, rdWinter;
+    private RadioButton rdOuter, rdTop, rdShirts, rdPants, rdSkirt, rdDress;
+    private ImageButton btnSelect, btnReset;
+    private String season = "";
+    private String cloth = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+        getSupportActionBar().setElevation(0);
+
+        PcGroup=findViewById(R.id.PcGroup);
+        rdSpring=findViewById(R.id.rdSpring);
+        rdSummer=findViewById(R.id.rdSummer);
+        rdAutumn=findViewById(R.id.rdAutumn);
+        rdWinter=findViewById(R.id.rdWinter);
+
+
+        CgGroup=findViewById(R.id.ClothGroup);
+        rdOuter=findViewById(R.id.rdOuter);
+        rdTop=findViewById(R.id.rdTop);
+        rdShirts=findViewById(R.id.rdShirts);
+        rdPants=findViewById(R.id.rdPants);
+        rdSkirt=findViewById(R.id.rdSkirt);
+        rdDress=findViewById(R.id.rdDress);
+
+
+        PcGroup.setVisibility(android.view.View.VISIBLE);
+        PcGroup.clearCheck();
+
+        CgGroup.setVisibility(android.view.View.VISIBLE);
+        CgGroup.clearCheck();
+
+
+        PcGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int i) {
+                if (i == R.id.rdSpring) {
+                    season = "봄";
+                } else if (i == R.id.rdSummer) {
+                    season = "여름";
+                } else if (i == R.id.rdAutumn) {
+                    season = "가을";
+                } else if (i == R.id.rdWinter) {
+                    season = "겨울";
+                }
+            }
+        });
+
+
+        CgGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int i) {
+                if (i == R.id.rdOuter) {
+                    cloth = "아우터";
+                } else if (i == R.id.rdTop) {
+                    cloth = "상의";
+                } else if (i == R.id.rdShirts) {
+                    cloth = "셔츠/블라우스";
+                } else if (i == R.id.rdPants) {
+                    cloth = "팬츠/데님";
+                }else if (i == R.id.rdSkirt) {
+                    cloth = "스커트";
+                }else if (i == R.id.rdDress) {
+                    cloth = "원피스";
+                }
+            }
+        });
+
+        btnReset=findViewById(R.id.btnReset);
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PcGroup.clearCheck();
+                CgGroup.clearCheck();
+                season = cloth = "";
+            }
+        });
+
+
+        btnSelect=findViewById(R.id.btnSelect);
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FilterActivity.this, com.example.color_fit.SearchResultActivity.class);
+
+                intent.putExtra("season", season);
+                intent.putExtra("cloth", cloth);
+
+                startActivity(intent);
+
+            }
+        });
+    }
+
+
+}
